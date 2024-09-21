04c74cd21e73d682bd6d81f6b87806106ba2e4e2
carmen <chg00016@red.ujaen.es>
2024-09-21 19:13:40 +0200

    Archivos de imgui introducidos y clase creada

---
+++ GUI/GUI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by carme on 21/09/2024.
+//
+
+#include "GUI.h"
---
+++ GUI/GUI.h
@@ -0,0 +1,16 @@
+//
+// Created by carme on 21/09/2024.
+//
+
+#ifndef GUI_H
+#define GUI_H
+
+
+
+class GUI {
+
+};
+
+
+
+#endif //GUI_H
