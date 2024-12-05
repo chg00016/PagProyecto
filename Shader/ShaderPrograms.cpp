a509d9946487b8996d247af569ea06ecee2c5594
carmen <chg00016@red.ujaen.es>
2024-12-05 20:44:03 +0100

    Arreglando errores pt2

--- Shader/ShaderPrograms.cpp
+++ Shader/ShaderPrograms.cpp
@@ -68,30 +68,26 @@ namespace PAG {
     //PR7
     void ShaderPrograms::aplicarSubrutina(const std::string &subrutina, GLuint tipo) {
          GLuint aux = glGetSubroutineIndex (idSP, tipo , subrutina.c_str());
-         if (aux == GL_INVALID_INDEX)
-             throw std::invalid_argument("ShaderPrograms::aplicarSubrutina: No existe la subrutina " + subrutina);
+
          glUniformSubroutinesuiv ( tipo, 1, &aux );
     }
 
     //PR8
     void ShaderPrograms::aplicarUniform(const std::string &uniform, float var) {
         GLint aux = glGetUniformLocation ( idSP, uniform.c_str() );
-        if (aux == GL_INVALID_INDEX)
-            throw std::invalid_argument("ShaderPrograms::aplicarUniform: No existe la variable " + uniform);;
+
         glUniform1f(aux, var);
     }
 
     void ShaderPrograms::aplicarUniform(const std::string &uniform, const glm::vec3 &vec) {
         GLint aux = glGetUniformLocation ( idSP, uniform.c_str() );
-        if (aux == GL_INVALID_INDEX)
-            throw std::invalid_argument("ShaderPrograms::aplicarUniform: No existe el vector " + uniform);;
+
         glUniform3f(aux, vec.x, vec.y, vec.z);
     }
 
     void ShaderPrograms::aplicarUniform(const std::string &uniform, const glm::mat4 &mat) {
         GLint aux = glGetUniformLocation ( idSP, uniform.c_str() );
-        if (aux == GL_INVALID_INDEX)
-            throw std::invalid_argument("ShaderPrograms::aplicarUniform: No existe la matriz " + uniform);;
+
         glUniformMatrix4fv(aux, 1, GL_FALSE, &mat[0][0]);
     }
 }
